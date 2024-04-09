d3c473e55019fcde512eb14f84c014f83f463684
M.Reza <40055314+mm-reza@users.noreply.github.com>
2024-04-09 12:41:00 -0600

    first

---
+++ src/App.js
@@ -0,0 +1,23 @@
+// import logo from './logo.svg';
+import './App.css';
+import MyQuiz from './components/Quiz';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Quiz
+        </p>
+
+
+        < MyQuiz/>
+
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
